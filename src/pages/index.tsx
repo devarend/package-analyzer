032e9009ed2f3d0bb831ed98d9b326374fcc8cb2
devarend <devarend@gmail.com>
2023-02-17 13:14:26 +0100

    created textarea with validation to check if dependencies are defined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/24/outline";
+import { ChangeEvent, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
+  const onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    try {
+      const parsedPackageJSON = JSON.parse(event.target.value);
+      if (!("dependencies" in parsedPackageJSON)) {
+        setValidationStatus("invalid");
+        return;
+      }
+      setValidationStatus("valid");
+    } catch {
+      setValidationStatus("invalid");
+    }
+  };
 
-export default function Home() {
+  const [validationStatus, setValidationStatus] = useState<
+    "valid" | "invalid" | null
+  >(null);
+
+  const valid = validationStatus === "valid";
+  const invalid = validationStatus === "invalid";
+
   return (
     <>
       <Head>
@@ -14,113 +34,44 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
+      <main>
+        <div className={"p-4"}>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+            <label
+              htmlFor="json"
+              className="block text-sm font-medium text-gray-700"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
+              Paste your package.json here
+            </label>
+            <div className="mt-1">
+              <textarea
+                onChange={onChange}
+                id="json"
+                name="json"
+                rows={12}
+                className="mt-1 p-2 block w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                placeholder="package.json"
               />
-            </a>
+            </div>
+            <div className={"flex mt-2 items-center"}>
+              {valid && (
+                <>
+                  <CheckCircleIcon className="h-6 w-6 text-green-600" />
+                  <p className="text-sm text-gray-500">Valid</p>
+                </>
+              )}
+              {invalid && (
+                <>
+                  <ExclamationCircleIcon className="h-6 w-6 text-red-600" />
+                  <p className="text-sm text-gray-500">Invalid</p>
+                </>
+              )}
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
-  )
-}
+  );
+};
+
+export default Home;
