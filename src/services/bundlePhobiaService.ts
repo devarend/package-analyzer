7444b79b93329e77cfc3dd2a721ef2e861b3685c
devarend <devarend@gmail.com>
2023-02-20 23:48:43 +0100

    service to fetch similar packages

--- src/services/bundlePhobiaService.ts
+++ src/services/bundlePhobiaService.ts
@@ -7,3 +7,13 @@ export const fetchPackageInformation = async (key: string, value: string) => {
   }
   return response.json();
 };
+
+export const fetchSimilarPackages = async (packageName: string) => {
+  const response = await fetch(
+    `https://bundlephobia.com/api/similar-packages?package=${packageName}`
+  );
+  if (!response.ok) {
+    throw new Error(`${response.status}: ${await response.text()}`);
+  }
+  return response.json();
+};
